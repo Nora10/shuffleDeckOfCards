96d9d2a71959c12ac76bf1864d0cafba80e090b7
Nora10 <noraiautt@gmail.com>
2024-11-27 15:56:16 -0600

    initial commit of the deck of card shuffler

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+import DeckShuffler from './deckOfCards';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h2> Deck of Cards Shuffler using react</h2>
+      <DeckShuffler />
+      </header>
+    </div>
+  );
+}
+
+export default App;
